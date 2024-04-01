ec984eb98cbb2056134e7d090c70660b9873e222
soar_local_admin <root@localhost>
2024-04-01 11:17:08 +0000

    Initial draft

--- Investigative - User Investigation.py
+++ Investigative - User Investigation.py
@@ -12,26 +12,26 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
 
-    # call 'format_query_test' block
-    format_query_test(container=container)
+    # call 'format_query_user_auth' block
+    format_query_user_auth(container=container)
 
     return
 
 @phantom.playbook_block()
-def run_query_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("run_query_1() called")
+def search_user_auth(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("search_user_auth() called")
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
-    format_query_test = phantom.get_format_data(name="format_query_test")
+    format_query_user_auth = phantom.get_format_data(name="format_query_user_auth")
 
     parameters = []
 
-    if format_query_test is not None:
+    if format_query_user_auth is not None:
         parameters.append({
             "command": "| tstats",
-            "search_mode": "smart",
-            "query": format_query_test,
+            "search_mode": "fast",
+            "query": format_query_user_auth,
             "display": "count",
             "start_time": 1534737603,
             "end_time": 1568916650,
@@ -47,16 +47,16 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["splunkes"], callback=debug_1)
+    phantom.act("run query", parameters=parameters, name="search_user_auth", assets=["splunkes"], callback=decide_user_auth)
 
     return
 
 
 @phantom.playbook_block()
-def format_query_test(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
-    phantom.debug("format_query_test() called")
+def format_query_user_auth(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_query_user_auth() called")
 
-    template = """count from datamodel=Authentication where Authentication.user=\"{0}\""""
+    template = """count from datamodel=Authentication where Authentication.user=\"{0}\"\n| eval result = if(count > 0, \"1\", \"0\")\n| fields result"""
 
     # parameter list for template variable replacement
     parameters = [
@@ -73,9 +73,9 @@ def format_query_test(action=None, success=None, container=None, results=None, h
     ## Custom Code End
     ################################################################################
 
-    phantom.format(container=container, template=template, parameters=parameters, name="format_query_test")
+    phantom.format(container=container, template=template, parameters=parameters, name="format_query_user_auth")
 
-    run_query_1(container=container)
+    search_user_auth(container=container)
 
     return
 
@@ -84,16 +84,14 @@ def format_query_test(action=None, success=None, container=None, results=None, h
 def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("debug_1() called")
 
-    run_query_1_result_data = phantom.collect2(container=container, datapath=["run_query_1:action_result.status","run_query_1:action_result.data.*.count","run_query_1:action_result.parameter.context.artifact_id"], action_results=results)
-
-    run_query_1_result_item_0 = [item[0] for item in run_query_1_result_data]
-    run_query_1_result_item_1 = [item[1] for item in run_query_1_result_data]
+    format_auth_yes = phantom.get_format_data(name="format_auth_yes")
+    format_auth_host_results = phantom.get_format_data(name="format_auth_host_results")
 
     parameters = []
 
     parameters.append({
-        "input_1": run_query_1_result_item_0,
-        "input_2": run_query_1_result_item_1,
+        "input_1": format_auth_yes,
+        "input_2": format_auth_host_results,
         "input_3": None,
         "input_4": None,
         "input_5": None,
@@ -115,6 +113,174 @@ def debug_1(action=None, success=None, container=None, results=None, handle=None
     ################################################################################
 
     phantom.custom_function(custom_function="community/debug", parameters=parameters, name="debug_1")
+
+    return
+
+
+@phantom.playbook_block()
+def decide_user_auth(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("decide_user_auth() called")
+
+    # check for 'if' condition 1
+    found_match_1 = phantom.decision(
+        container=container,
+        conditions=[
+            ["search_user_auth:action_result.data.*.result", ">", 0]
+        ],
+        delimiter=None)
+
+    # call connected blocks if condition 1 matched
+    if found_match_1:
+        format_auth_yes(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'else' condition 2
+    format_auth_no(action=action, success=success, container=container, results=results, handle=handle)
+
+    return
+
+
+@phantom.playbook_block()
+def format_auth_yes(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_auth_yes() called")
+
+    template = """Has user {0} has been active in the environment in the last seven days?\n**Yes**\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "artifact:*.cef.suser"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_auth_yes")
+
+    format_query_auth_hosts(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def format_auth_no(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_auth_no() called")
+
+    template = """Has user {0} has been active in the environment in the last seven days?\n**No**"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        ""
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_auth_no")
+
+    return
+
+
+@phantom.playbook_block()
+def format_query_auth_hosts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_query_auth_hosts() called")
+
+    template = """dc(host) as host_count values(host) as host_list from datamodel=Authentication where Authentication.user=\"{0}\" by host"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "artifact:*.cef.suser"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_query_auth_hosts")
+
+    search_user_auth_hosts(container=container)
+
+    return
+
+
+@phantom.playbook_block()
+def search_user_auth_hosts(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("search_user_auth_hosts() called")
+
+    # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
+
+    format_query_auth_hosts = phantom.get_format_data(name="format_query_auth_hosts")
+
+    parameters = []
+
+    if format_query_auth_hosts is not None:
+        parameters.append({
+            "command": "| tstats",
+            "search_mode": "fast",
+            "query": format_query_auth_hosts,
+            "start_time": 1534737603,
+            "end_time": 1568916650,
+        })
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.act("run query", parameters=parameters, name="search_user_auth_hosts", assets=["splunkes"], callback=format_auth_host_results)
+
+    return
+
+
+@phantom.playbook_block()
+def format_auth_host_results(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
+    phantom.debug("format_auth_host_results() called")
+
+    template = """How many hosts has the user logged onto in the time period?\n**{0}**\n\nWhat were the unique hosts involved?\n**{1}**\n"""
+
+    # parameter list for template variable replacement
+    parameters = [
+        "search_user_auth_hosts:action_result.data.*.host_count",
+        "search_user_auth_hosts:action_result.data.*.host_list"
+    ]
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    phantom.format(container=container, template=template, parameters=parameters, name="format_auth_host_results")
+
+    debug_1(container=container)
 
     return
 
