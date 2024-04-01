7fbaaa7a92782e5b4134d585f2bc30617492b9c0
soar_local_admin <root@localhost>
2024-04-01 10:06:02 +0000

    Initial draft

---
+++ Investigative - User Investigation.py
@@ -0,0 +1,33 @@
+"""
+
+"""
+
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+
+@phantom.playbook_block()
+def on_start(container):
+    phantom.debug('on_start() called')
+
+
+
+    return
+
+@phantom.playbook_block()
+def on_finish(container, summary):
+    phantom.debug("on_finish() called")
+
+    ################################################################################
+    ## Custom Code Start
+    ################################################################################
+
+    # Write your custom code here...
+
+    ################################################################################
+    ## Custom Code End
+    ################################################################################
+
+    return
