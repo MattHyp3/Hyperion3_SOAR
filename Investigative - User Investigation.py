b8d490909bdb3bc5d2c3f52cbaa8f48d356d80fc
soar_local_admin <root@localhost>
2024-04-01 10:19:19 +0000

    Initial draft

--- Investigative - User Investigation.py
+++ Investigative - User Investigation.py
@@ -23,13 +23,16 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
 
     # phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
 
+    format_query_test = phantom.get_format_data(name="format_query_test")
+
     parameters = []
 
-    parameters.append({
-        "command": "search",
-        "search_mode": "smart",
-        "query": "",
-    })
+    if format_query_test is not None:
+        parameters.append({
+            "command": "| makeresults",
+            "search_mode": "smart",
+            "query": format_query_test,
+        })
 
     ################################################################################
     ## Custom Code Start
@@ -41,7 +44,7 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
     ## Custom Code End
     ################################################################################
 
-    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["splunkes"])
+    phantom.act("run query", parameters=parameters, name="run_query_1", assets=["splunkes"], callback=debug_1)
 
     return
 
@@ -50,7 +53,7 @@ def run_query_1(action=None, success=None, container=None, results=None, handle=
 def format_query_test(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("format_query_test() called")
 
-    template = """| makeresults \n| eval test=\"test\"\n"""
+    template = """| eval test=\"test\"\n| table test"""
 
     # parameter list for template variable replacement
     parameters = []
@@ -67,7 +70,7 @@ def format_query_test(action=None, success=None, container=None, results=None, h
 
     phantom.format(container=container, template=template, parameters=parameters, name="format_query_test")
 
-    debug_1(container=container)
+    run_query_1(container=container)
 
     return
 
@@ -76,14 +79,16 @@ def format_query_test(action=None, success=None, container=None, results=None, h
 def debug_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None, custom_function=None, loop_state_json=None, **kwargs):
     phantom.debug("debug_1() called")
 
-    format_query_test = phantom.get_format_data(name="format_query_test")
-    format_query_test__as_list = phantom.get_format_data(name="format_query_test__as_list")
+    run_query_1_result_data = phantom.collect2(container=container, datapath=["run_query_1:action_result.status","run_query_1:action_result.data","run_query_1:action_result.parameter.context.artifact_id"], action_results=results)
+
+    run_query_1_result_item_0 = [item[0] for item in run_query_1_result_data]
+    run_query_1_result_item_1 = [item[1] for item in run_query_1_result_data]
 
     parameters = []
 
     parameters.append({
-        "input_1": format_query_test,
-        "input_2": format_query_test__as_list,
+        "input_1": run_query_1_result_item_0,
+        "input_2": run_query_1_result_item_1,
         "input_3": None,
         "input_4": None,
         "input_5": None,
